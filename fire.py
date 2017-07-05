074263c0e3968c5276fc3374721c6b8119f122ed
apex1000 <siddhartsingh9876@gmail.com>
2017-07-05 19:30:07 +0530

    firebase update

--- fire.py
+++ fire.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 import json
 from firebase import firebase
 import datetime
@@ -8,33 +9,23 @@ firebase = firebase.FirebaseApplication('https://haha-4cf04.firebaseio.com', Non
 data = []
 
 #LEYENDO INFO DEL CSV 
-with open('/mnt/d/DMS/yourBigFile3.txt')as f:
-	read = f.readlines()
+with open('/mnt/d/offlinemcs/yourBigFile3.csv')as csvfile:
+	read = csv.reader(csvfile)
 	i=0
 	for row in read:
-		size =(row[0])
-		type = (row[1])
-		tlt = (row[2])	
-		category =(row[3])
-		sender = (row[4])
-		reciver = (row[5])
-		lat = (row[6])
-		long1 = (row[7])
-		datetime = (row[8])
-		group =(row[9])
-		name =1
+		size =int(row[0])
+		type = (row[1]).replace(" ","")
+		tlt = int(row[2])	
+		category =(row[3]).replace(" ","")
+		sender = int(row[4])
+		reciver = str(row[5]).replace(" ","")
+		lat = float(row[6])
+		long1 = float(row[7])
+		datetime = int(row[8])
+		group =int(row[9])
+		name =str(str(type)+'_'+str(tlt)+'_'+str(category)+'_'+str(sender)+'_'+str(reciver)+'_'+str(lat)+'_'+str(long1)+'_'+str(datetime)+'_'+str(group))
+		text="NULL"
 		#dateTime = datetime.datetime.strptime(dateTime, "%Y/%m/%d %H:%M:%S")
 		#print str(dateTime1) + '->' + str(dateTime)
-		print name
-		print size
-		print type
-		print tlt
-		print category
-		print sender
-		print reciver
-		print lat
-		print long1
-		print datetime
-		print group
 		i=i+1
 		firebase.put('/onlineMCS',i,{"name": name,"type": type,"tlt": tlt,"category": category,"sender": sender,"reciver": reciver,"lat": lat,"long": long1,"datetime": datetime, "group": group,"size":size})
